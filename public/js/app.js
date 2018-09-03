c8380686dc92c2dfa29a4a88b9bc73717ff38b8d
Prince Batra <princebatra2315@gmail.com>
2018-09-03 10:19:49 +0530

    Final commit

--- public/js/app.js
+++
@@ -1,4 +0,0 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles', 'assets/particles-config.json', function() {
-  console.log('callback - particles.js config loaded');
-});
